f9a370b15fc4c1d7836783f4c39a1fe2964b241a
Austin Elwell <austinelwell99@gmail.com>
2021-03-22 18:30:33 -0500

    test again

--- Products/Database/Index.js
+++ Products/Database/Index.js
@@ -5,11 +5,7 @@ const mongoose = require('mongoose');
 //   useUnifiedTopology: true,
 // });
 
-var MongoDB = mongoose.connect('mongodb://172.31.14.203:27017/sdc',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }).connection
+var MongoDB = mongoose.connect('mongodb://172.31.14.203:27017/sdc').connection
 MongoDB.on('error', function(err) { console.log(err.message); });
 MongoDB.once('open', function() {
   console.log("mongodb connection open");
