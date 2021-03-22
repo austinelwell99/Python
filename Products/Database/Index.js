a2cad811e680059922757be68039d1df7d255790
Austin Elwell <austinelwell99@gmail.com>
2021-03-22 18:34:25 -0500

    Reverted test changes

--- Products/Database/Index.js
+++ Products/Database/Index.js
@@ -1,14 +1,8 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect('mongodb://172.31.14.203:27017/sdc', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
-
-var MongoDB = mongoose.connect('mongodb://172.31.14.203:27017/sdc').connection
-MongoDB.on('error', function(err) { console.log(err.message); });
-MongoDB.once('open', function() {
-  console.log("mongodb connection open");
+mongoose.connect('mongodb://172.31.14.203:27017/sdc', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 const featureSchema = new mongoose.Schema({
