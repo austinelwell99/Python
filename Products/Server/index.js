e169201915f85689a36cf3a7f0d1c23823fcce0e
Austin Elwell <austinelwell99@gmail.com>
2021-03-24 13:05:29 -0500

    Added limit for count

--- Products/Server/index.js
+++ Products/Server/index.js
@@ -8,7 +8,8 @@ const db = require('../Database/Index.js');
 app.use(express.json())
 
 app.get('/products', (req, res) => {
-  const count = req.query?.count || 5
+  const count = req.query?.count|| 5
+  if (count > 500) count = 500
   const skip = (req.query?.page || 1) * count - count
   return db.sendAll(count, skip)
     .then(data => {
