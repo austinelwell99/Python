ca986354e64bb6fd4cb95138c9c052e769efb50d
Austin Elwell <austinelwell99@gmail.com>
2021-03-24 12:47:06 -0500

    Adding console.log

--- Products/Server/index.js
+++ Products/Server/index.js
@@ -8,8 +8,9 @@ const db = require('../Database/Index.js');
 app.use(express.json())
 
 app.get('/products', (req, res) => {
-  const count = req.query.count || 5
-  const skip = (req.query.page || 1) * count - count
+  console.log(req.query)
+  const count = req.query?.count || 5
+  const skip = (req.query?.page || 1) * count - count
   return db.sendAll(count, skip)
     .then(data => {
       const output = []
